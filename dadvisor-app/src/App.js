32d8390103c1b212741ea101eeb507be7b4bcdfb
Bobby Liberti <BobbyLiberti@Mes-iMac.local>
2019-09-14 18:17:32 -0400

    added user component

---
+++ dadvisor-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
+
+import Users from './components/Users';
+
+
+function App() {
+
+  return (
+    <div className="App">
+        <Users />
+    </div>
+  );
+}
+
+export default App;
---
+++ dadvisor-app/src/components/Users.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+
+const User = () => {
+
+    return (
+        <>
+        <div>
+            <h1>Hello from user.js</h1>
+        </div>
+        </>
+    )
+}
+
+export default User;
